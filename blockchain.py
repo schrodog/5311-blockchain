b2aa6829b7d310ae695c4143bf88c26d6e634021
schrodog <sky_sun_tg@hotmail.com>
2018-11-24 12:37:56 +0800

    fix bug

--- blockchain.py
+++ blockchain.py
@@ -35,20 +35,17 @@ class Blockchain:
     # 'current_hash': 0x000dc75a315c77a1f9c98fb6247d03dd18ac52632d7dc6a9920261d8109b37cf, 
     # 'nonce': 0, 'transaction':[], 'merkle_root': [], 'index': 0})
     self.blocks = [Block.genesis()]
-    self.diffculty = 1
+    self.diffculty = 3
 
-  def get(self):
+  @property
+  def blocks(self):
     return self.blocks
 
   def latest_block(self):
     return self.blocks[-1]
   
-  def is_valid_hash_difficulty(self, hash):
+  def check_difficulty(self, hash):
     return hash[:self.difficulty] == '0'*self.diffculty
-
-  def calculate_hash_for_block(self, block):
-    # TODO join block's transactions as data
-    return self.calculate_hash(block.index, block.prev_hash, block.timestamp, data, block.nonce)
 
   def calculate_hash(self, previousHash, timestamp, nonce):
     text = previousHash + str(timestamp) + str(nonce)
@@ -57,16 +54,16 @@ class Blockchain:
     return s.hexdigest()
   
   def mine(self):
-    new_block = self.generate_next_block()
+    new_block = self.create_next_block()
     self.add_block(new_block)
   
-  def generate_next_block(self):
+  def create_next_block(self):
     next_index = self.latest_block().index + 1
     previous_hash = self.latest_block().current_hash
     timestamp = time.time()
     nonce = 0
     next_hash = self.calculate_hash(previous_hash, timestamp, nonce)
-    while not self.is_valid_hash_difficulty(next_hash):
+    while not self.check_difficulty(next_hash):
       nonce += 1
       timestamp = time.time()
       next_hash = self.calculate_hash(previous_hash, timestamp, nonce)
@@ -76,25 +73,25 @@ class Blockchain:
     return next_block
   
   def add_block(self, new_block):
-    if self.is_valid_next_block(new_block, self.latest_block()):
+    if self.check_next_block(new_block, self.latest_block()):
       self.blocks.append(new_block)
     else:
       raise Exception('invalid block') 
   
-  def is_valid_next_block(self, nextBlock, previousBlock):
-    nextBlockHash = self.calculate_hash_for_block(nextBlock)
+  def check_next_block(self, nextBlock, previousBlock):
+    nextBlockHash = self.calculate_hash(nextBlock.prev_hash, nextBlock.timestamp, nextBlock.nonce)
     if (previousBlock.index + 1) != nextBlock.index:
       return False
     elif previousBlock.current_hash != nextBlock.prev_hash:
       return False
     elif nextBlockHash != nextBlock.current_hash:
       return False
-    elif not self.is_valid_hash_difficulty(nextBlockHash):
+    elif not self.check_difficulty(nextBlockHash):
       return False
     else:
       return True
 
-  def isValidChain(self, chain):
+  def check_chain(self, chain):
     if chain[0] != Block.genesis():
       return False
     
@@ -107,8 +104,7 @@ class Blockchain:
     
     return True
 
-# isChainLonger(self, chain)
-# replaceChain(self, newChain)
+  def replaceChain(self, newChain)
 
   
 
