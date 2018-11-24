2701cf8dbcbe42bc5b6b10ab60a5a3a03e018bce
kybettylee <ky.betty.lee@gmail.com>
2018-11-24 11:35:39 +0800

    Add more function in blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,6 @@
+from hashlib import sha256
+import time
+
 class Transaction:
   def __init__(self, t):
     self.timestamp = t['timestamp']
@@ -15,27 +18,98 @@ class Block:
     self.nonce = b['nonce']
     self.transaction = b['transaction']
     self.merkle_root = b['merkle_root']
+    self.index = b['index']
+  
+  def genesis(self):
+    self.timestamp = time.time()
+    self.prev_hash = 0x0
+    self.current_hash = 0x000dc75a315c77a1f9c98fb6247d03dd18ac52632d7dc6a9920261d8109b37cf
+    self.nonce = 0
+    self.transaction = []
+    self.merkle_root = 0x0,
+    self.index = 0
 
 class Blockchain:    
   def __init__(self):
-    self.blocks = []
-    self.diffculty = 24;
+    #genesis = Block({'timestamp': time.time(), 'prev_hash': 0x0, 'current_hash': 0x000dc75a315c77a1f9c98fb6247d03dd18ac52632d7dc6a9920261d8109b37cf, 'nonce': 0, 'transaction':[], 'merkle_root': [], 'index': 0})
+    self.blocks = [Block.genesis()]
+    self.diffculty = 1
 
-  def get()
+  def get(self):
     return self.blocks
 
   def latest_block(self):
     return self.blocks[-1]
   
-  def is_valid_hash_diffculty(hash):
-    for i in range(0, len(a)){
+  def is_valid_hash_difficulty(self, hash):
+    for i in range(0, len(hash)):
       if hash[i] != 0:
         break
-    }
-    return i >= self.diffculty;
+    return i >= self.diffculty
+
+  def calculate_hash_for_block(self, block):
+    # TODO join block's transactions as data
+    data = 0
+    return self.calculate_hash(block.index, block.prev_hash, block.timestamp, data, block.nonce)
+
+  def calculate_hash(self, index, previousHash, timestamp, data, nonce):
+    s = sha256()
+    s.update((str(timestamp)[0:10]).encode())
+    return s.hexdigest()
   
+  def mine(self, data):
+    new_block = self.generate_next_block(data)
+    self.add_block(new_block)
+  
+  def generate_next_block(self, data):
+    next_index = self.latest_block().index + 1
+    previous_hash = self.latest_block().current_hash
+    timestamp = time.time()
+    nonce = 0
+    next_hash = self.calculate_hash(next_index, previous_hash, timestamp, data, nonce)
+    while (not self.is_valid_hash_difficulty(next_hash)):
+      nonce = nonce + 1
+      timestamp = time.time()
+      next_hash = self.calculate_hash(next_index, previous_hash, timestamp, data, nonce)
+    
+    # What is correct value for merkle root and transaction?
+    next_block = Block({'index':next_index, 'prev_hash':previous_hash, 'timestamp':timestamp, 'transaction': data, 'current_hash':next_hash, 'nonce': nonce, 'merkle_root':[]})
+    return next_block
+  
+  def add_block(self, new_block):
+    if (self.is_valid_next_block(new_block, self.latest_block())):
+      self.blocks.append(new_block)
+    else:
+      raise Exception('invalid block') 
+  
+  def is_valid_next_block(self, nextBlock, previousBlock):
+    nextBlockHash = self.calculate_hash_for_block(nextBlock)
+    if (previousBlock.index + 1 != nextBlock.index):
+      return False
+    elif (previousBlock.current_hash != nextBlock.prev_hash):
+      return False
+    elif (nextBlockHash != nextBlock.current_hash):
+      return False
+    elif (not self.is_valid_hash_difficulty(nextBlockHash)):
+      return False
+    else:
+      return True
 
+  isValidChain(self, chain):
+    if (chain[0] != Block.genesis()):
+      return False
+    
+    tempChain = [chain[0]]
+    for i in range(1, len(chain)):
+      if (self.isValidNextBlock(chain[i], tempChain[i - 1])) :
+        tempChain.append(chain[i])
+      else :
+        return False
+    
+    return True
 
+# isChainLonger(self, chain)
+# replaceChain(self, newChain)
 
   
 
